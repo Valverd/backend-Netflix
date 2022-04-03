47b78b28821ca85249cb232de576bc4c1f8a74fa
Valverd <renanvgmartins@hotmail.com>
2022-04-03 00:08:16 -0300

    edições

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,8 +8,8 @@ router.post('/register', async (req, res) => {
 
     const selectedUser = await User.findOne({ email: req.body.email });
     if (selectedUser) {
-        return res.send('Email já cadastrado!');
-    }
+        return res.status(400).send('Email já cadastrado!');
+    };
 
     let user = new User({
         name: req.body.name,
@@ -31,12 +31,12 @@ router.post('/login', async (req, res) => {
 
     const selectedEmail = await User.findOne({ email: req.body.email });
     if (!selectedEmail) {
-        return res.send("Email ou Senha digitados incorretamente!");
+        return res.status(404).send("Email ou Senha digitados incorretamente!");
     };
 
     const selectedPassword = await User.findOne({ password: req.body.password });
     if (!selectedPassword) {
-        return res.send("Email ou Senha digitados incorretamente!");
+        return res.status(404).send("Email ou Senha digitados incorretamente!");
     };
 
     const token = jwt.sign({ name: selectedEmail.name, email: selectedEmail.email, admin: selectedEmail.admin }, process.env.TOKEN_SECRET, {expiresIn: '24h'});
